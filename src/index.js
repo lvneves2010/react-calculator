92dc94d7de2805a3e5dae56265429084c2390812
leonardo neves <leonardo.neves@ibm.com>
2020-08-25 12:52:07 -0300

    final version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './main/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculator</h1>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
